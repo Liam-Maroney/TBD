ddd2cd61c604481a8bdcd06edf2ac9f41a99dbfc
Liam <liammaroney89@gmail.com>
2023-09-19 15:12:55 +0200

    feat: character created

--- main.cs
+++ main.cs
@@ -3,6 +3,12 @@ using System;
 
 public partial class main : Node2D
 {
+	
+	private void _on_play_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://world.tscn");
+	}
+
 	private void _on_quit_pressed()
 	{
 		GetTree().Quit();
